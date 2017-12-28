b16149790871a240e166b041770010776704c7b0
tongzezhu <tongze.zhu@westwell-lab.com>
2017-12-28 15:01:42 +0800

    modify small things on tcp_client

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -3,6 +3,7 @@
 #include <sys/time.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #include <string>
 #include <cstring>
@@ -14,21 +15,21 @@ int main()
   int handle = 0;
   int ret = 0;
   std::string strIP("127.0.0.1");
-  static const int nPort = 50000;
+  static const short nPort = 9000;
   sockaddr_in addr;
 
-  handle = socket(AF_INET, SOCK_STREAM, 0);
+  handle = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0);
 
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
   inet_aton(strIP.c_str(), &addr.sin_addr);
-  addr.sin_port = nPort;
-  ret = connect(handle, (sockaddr*)(&addr), sizeof(sockaddr_in));
+  addr.sin_port = htons(nPort);
+  ret = connect(handle, (struct sockaddr*)(&addr), sizeof(addr));
 
   if(ret < 0)
     std::cout << strerror(errno) << std::endl;
     //perror("conn error");
-  shutdown(handle, SHUT_RDWR);
+  close(handle);
 
   return 0;
 }
